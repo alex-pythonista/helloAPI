7672d5551c88a90c222ce0e9b73440e6bc0c2f2f
alex-pythonista <indrajit.alex@gmail.com>
2022-10-26 01:39:36 +0600

    SECRET_KEY reinstalled

--- hello/settings.py
+++ hello/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,15 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-try:
-    SECRET_KEY = os.environ["SECRET_KEY"]
-except KeyError as e:
-    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
+
+SECRET_KEY = 'django-insecure-iibxfs4_#nughvl=&-n9--7#h+mco%30hq+492fb57z%3$jqp!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".alexpy.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
