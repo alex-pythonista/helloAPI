3d8ff0ab2b0566bb555edd592c158cff7b8cd90f
alex-pythonista <indrajit.alex@gmail.com>
2022-10-25 16:30:53 +0600

    project init

---
+++ hello/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+# from django.shortcuts import render
+def hello(request):
+    return HttpResponse(
+        """
+        <h1>Hello World! This is AlexPy.</h1>
+        """
+    )
